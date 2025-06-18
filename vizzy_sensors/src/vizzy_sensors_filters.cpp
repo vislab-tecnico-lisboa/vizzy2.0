7da11c3f01dfcaac09a9ebacb596ccf6785baf2c
JPLDevMaster <joao.penha.lopes@tecnico.ulisboa.pt>
2025-06-18 14:56:59 +0100

    Added necessary code and refactored CMakeLists.txt for package vizzy_sensors.

---
+++ vizzy_sensors/src/vizzy_sensors_filters.cpp
@@ -0,0 +1,15 @@
+/*
+ * LICENSE: https://github.com/utexas-bwi/segbot/blob/devel/LICENSE
+ */
+
+#include "vizzy_sensors/footprint_filter.h"
+#include "vizzy_sensors/nan_to_inf_filter.h"
+#include "vizzy_sensors/scan_angle_filter.h"
+#include "sensor_msgs/LaserScan.h"
+#include "filters/filter_base.h"
+
+#include "pluginlib/class_list_macros.h"
+
+PLUGINLIB_EXPORT_CLASS(vizzy_sensors::VizzyFootprintFilter, filters::FilterBase<sensor_msgs::LaserScan>)
+PLUGINLIB_EXPORT_CLASS(vizzy_sensors::NanToInfFilter, filters::FilterBase<sensor_msgs::LaserScan>)
+PLUGINLIB_EXPORT_CLASS(vizzy_sensors::ScanAngleFilter, filters::FilterBase<sensor_msgs::LaserScan>)
