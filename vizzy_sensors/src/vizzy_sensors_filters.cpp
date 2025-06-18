fc02bcf440a3252b9322bdaca5a068598a869cd4
JPLDevMaster <joao.penha.lopes@tecnico.ulisboa.pt>
2025-06-18 15:15:21 +0100

    Refactored CPP code from ROS1 to ROS2.

--- vizzy_sensors/src/vizzy_sensors_filters.cpp
+++ vizzy_sensors/src/vizzy_sensors_filters.cpp
@@ -6,9 +6,8 @@
 #include "vizzy_sensors/scan_angle_filter.h"
 #include "sensor_msgs/msg/laser_scan.hpp"
 #include "filters/filter_base.hpp"
+#include <pluginlib/class_list_macros.hpp>
 
-#include "pluginlib/class_list_macros.h"
-
-PLUGINLIB_EXPORT_CLASS(vizzy_sensors::VizzyFootprintFilter, filters::FilterBase<sensor_msgs::LaserScan>)
-PLUGINLIB_EXPORT_CLASS(vizzy_sensors::NanToInfFilter, filters::FilterBase<sensor_msgs::LaserScan>)
-PLUGINLIB_EXPORT_CLASS(vizzy_sensors::ScanAngleFilter, filters::FilterBase<sensor_msgs::LaserScan>)
+PLUGINLIB_EXPORT_CLASS(vizzy_sensors::VizzyFootprintFilter, filters::FilterBase<sensor_msgs::msg::LaserScan>)
+PLUGINLIB_EXPORT_CLASS(vizzy_sensors::NanToInfFilter, filters::FilterBase<sensor_msgs::msg::LaserScan>)
+PLUGINLIB_EXPORT_CLASS(vizzy_sensors::ScanAngleFilter, filters::FilterBase<sensor_msgs::msg::LaserScan>)
