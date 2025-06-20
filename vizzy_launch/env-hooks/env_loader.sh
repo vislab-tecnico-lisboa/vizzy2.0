1e1b326692acc4898e2fa6830b251341f745e8fa
JPLDevMaster <joao.penha.lopes@tecnico.ulisboa.pt>
2025-06-20 12:20:55 +0100

    Added ROS_MASTER export in env loader.

--- vizzy_launch/env-hooks/env_loader.sh
+++ vizzy_launch/env-hooks/env_loader.sh
@@ -1,2 +1,4 @@
 export GAZEBO_MODEL_PATH=$ROS2_WS/src/vizzy/vizzy_gazebo:\$GAZEBO_MODEL_PATH
 export IGN_GAZEBO_RESOURCE_PATH=$(ros2 pkg prefix vizzy_gazebo)/share:$(ros2 pkg prefix vizzy_description)/share:$IGN_GAZEBO_RESOURCE_PATH
+export ROS_MASTER_URI=http://10.1.3.1:11311
+export ROS_IP=10.1.3.1
